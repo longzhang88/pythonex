74b3f8fba167b7d341d4d041e8981687bf8b718d
Long Zhang <longzhang.cav@gmail.com>
2016-05-27 16:53:53 -0700

    read *.png and add a input number on the top right

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pythonex
 source code of show_me_the_code python projects
+
+Update May.27.2016
+No.000 : avatar_add_num.py
+
+install pip
+
+
+
--- src/q000/avatar_add_num.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-https://github.com/Show-Me-the-Code/show-me-the-code
-No.0000
-read a picture and add number at the top right
-"""
-
